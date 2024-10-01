2d46f6a92f4992d9691f93a4344aa6d10e3a2c93
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2024-10-01 18:10:01 +0000

    add engine list

--- index.js
+++ index.js
@@ -3,6 +3,45 @@ import process from 'node:process';
 
 const litoken = process.env.LICHESS_TOKEN
 
+const seed = [ // maybe need update
+    "TBA", // 0
+    "Stockfish", // 1
+    "LCZero", // 2
+    "Ceres", // 3
+    "Berserk", // 4
+    "Revenge",// 5
+    "Obsidian", // 6
+    "Ethereal", // 7
+    "KomodoDragon", // 8
+
+    // RANDOM SEED
+
+    "Lizard", // 9 - r01
+    "Stormphrax", // 10 - r02
+    "RubiChess", // 11 - r03
+    "Caissa", // 12 - r04
+    "Tucano", // 13 - r05 
+    "Seer", // 14 - r06
+    "Velvet", // 15 - r07
+    "Uralochka", // 16 - r08
+    "Texel", // 17 - r09
+    "Ginkgo", // 18 - r10
+    "Minic", // 19 - r11
+    "Altair", // 20 - r12
+    "Stoofvlees", // 21 - r13
+    "DeepSjeng", // 22 - r14
+    "Arasan", // 23 - r15
+    "BlackMarlin", // 24 - r16
+    "Equisetum", // 25 - r17
+    "akimbo", // 26 - r18
+    "Booot", // 27 - r19
+    "PlentyChess", // 28 - r20
+    "Renegade", // 29 - r21
+    "Igel", // 30 - r22
+    "Viridithas", // 31 - r23
+    "Devre", // 32 - r24
+]
+
 const paring = [
     [["Stockfish_15_100M", "Obsidian dev-13.23"], "vu0hvi1p"],
     [["Stockfish_15_100M", "LCZero 0.31-dag-4167c1e-BT4-6147500-it332"], "wulYcqx9"],
