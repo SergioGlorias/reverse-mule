6b4e74180327ea5f6ebe064a0944451d793cd28c
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2024-10-06 10:06:33 +0100

    update paring

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ const paring = [
   [[seed[18], seed[19]], "ZpXSiTaf"], // Match 4
   [[seed[2], seed[23]], "qNm2Urxl"], // Match 5
   [[seed[6], seed[26]], "ElBZMblR"], // Match 6
-  [[seed[4], seed[0]], "JykQLa3c"], // Match 7
-  [[seed[0], seed[0]], "Vmnr4qJW"], // Match 8
+  [[seed[4], seed[28]], "JykQLa3c"], // Match 7
+  [[seed[8], seed[0]], "Vmnr4qJW"], // Match 8
 
   // Quarterfinals
   [[seed[0], seed[0]], "vOrgdhPg"], // Match 1
