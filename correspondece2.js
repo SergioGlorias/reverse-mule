2da1c71fcee9ecfb80e235ff4de4c6dc2802c728
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2025-05-04 22:58:19 +0000

    correspondece 2

---
+++ correspondece2.js
@@ -0,0 +1,71 @@
+import { parsePgn, makePgn } from "chessops/pgn";
+
+const iccfurl = "https://www.iccf.com/GetEventPGN.aspx?id=109591";
+
+const roundsIds = ["R6bBTJ68"];
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const litoken = process.env.LICHESS_TOKEN;
+
+const getPGN = () =>
+  fetch(iccfurl, {
+    headers: {
+      "User-Agent": "github.com/SergioGlorias/reverse-mule",
+    },
+  })
+    .then((res) => (res.status === 200 ? res.text() : ""))
+    .then((text) => parsePgn(text))
+    .catch(() => null);
+
+const divideGamesByRounds = (games, roundsCount) => {
+  const groups = Array.from({ length: roundsCount }, () => []);
+  games.forEach((game, index) => {
+    groups[index % roundsCount].push(game);
+  });
+  return groups;
+};
+
+const pushPGN = (pgn, id) => {
+  return fetch(`https://lichess.org/api/broadcast/round/${id}/push`, {
+    body: pgn,
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + litoken,
+      "User-Agent": "Reverse Mule by SergioGlorias/reverse-mule",
+    },
+  })
+    .then((res) => res.json())
+    .catch(() => null);
+};
+
+const run = async () => {
+  const games = await getPGN();
+  if (!games) return;
+
+  const roundsCount = roundsIds.length;
+  if (roundsCount === 0) return;
+  const groupedGames = divideGamesByRounds(games, roundsCount);
+
+  for (let i = 0; i < roundsCount; i++) {
+    const roundGames = groupedGames[i];
+    const roundId = roundsIds[i];
+
+    const gamesPGN = roundGames
+      .map((game) => {
+        game.headers.set("WhiteFideId", "0");
+        game.headers.set("BlackFideId", "0");
+        return makePgn(game);
+      })
+      .join("\n\n");
+
+    const r = await pushPGN(gamesPGN, roundId);
+
+    if (r) console.info(r);
+    else console.error("Fail Push");
+
+    await delay(1000);
+  }
+};
+run();
+// 5 minutes
+setInterval(() => run(), 5 * 60 * 1000);
